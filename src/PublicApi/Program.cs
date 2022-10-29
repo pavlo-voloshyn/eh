89abf845e48165f51395156ddc86bfb608dc6fa7
Павло <pavlo.voloshyn.pz.2020@lpnu.ua>
2022-10-30 00:05:05 +0300

    ve3

--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -76,7 +76,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: CORS_POLICY,
                       corsPolicyBuilder =>
                       {
-                          corsPolicyBuilder.WithOrigins(baseUrlConfig.WebBase.Replace("host.docker.internal", "localhost").TrimEnd('/'));
+                          corsPolicyBuilder.WithOrigins(baseUrlConfig.WebBase);
                           corsPolicyBuilder.AllowAnyMethod();
                           corsPolicyBuilder.AllowAnyHeader();
                       });
